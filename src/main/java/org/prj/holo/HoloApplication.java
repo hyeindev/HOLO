cddc18d48f41700e5b5a2a9d0bbd7e085cab8e7e
hyeindev <67957992+hyeindev@users.noreply.github.com>
2022-10-13 10:56:29 +0900

    create project

---
+++ src/main/java/org/prj/holo/HoloApplication.java
@@ -0,0 +1,13 @@
+package org.prj.holo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoloApplication.class, args);
+    }
+
+}
